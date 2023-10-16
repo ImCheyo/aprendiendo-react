7c5071391bbf41f34482992fe92e5c10c5a913d6
Beneharo <beneharodm@gmail.com>
2023-10-16 14:36:50 +0100

    Terminado

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,21 +1,47 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App() {
-    const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+    const [fact, setFact] = useState()
+    const [imageUrl, setImageUrl] = useState()
+    const [factError, setFactError] = useState()
 
     useEffect(() => {
-        fetch('https://catfact.ninja/fact')
-        .then(res => res.json())
-        .then(data => setFact(data.fact))
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+            .then(res => {
+                if (!res.ok) {
+                    setFactError('No se ha podido recuperar la cita')
+                }
+                return res.json()
+            })
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
+            })
     }, [])
+
+    useEffect(() => {
+        if (!fact) return
+
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+            .then(res => res.json())
+            .then(response => {
+                const { url } = response
+                setImageUrl(url)
+            })
+    }, [fact])
 
     return (
         <main>
             <h1>App de gatitos</h1>
-            <p>{fact}</p>
+            {fact && <p>{fact}</p>}
+            {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
         </main>
     )
 }
